e82de87e04c8da91522f466a07e1517709ad7021
NoahMast28 <noahmast28@gmail.com>
2025-02-24 22:38:59 -0600

    Edited practice problem 1 and completed practice problem 3

---
+++ Practice3/script.js
@@ -0,0 +1,11 @@
+function verifyPassword() {
+    const password1 = document.getElementById('password1').value;
+    const password2 = document.getElementById('password2').value;
+    if (password1.length < 8 || password2.length < 8) {
+        alert("Password must be at least 8 characters long.");
+    } else if (password1 !== password2) {
+        alert("Passwords do not match.");
+    } else {
+        alert("Passwords match!");
+    }
+}
